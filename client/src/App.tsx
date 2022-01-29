5e33e3f99c9899093a47716fc54c434399e93a60
Justas Belevicius <jube1997@gmail.com>
2022-01-29 14:57:13 +0200

    initial

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,13 @@ import StateHandler from './StateHandler';
 import states, { State } from './States';
 import { WebSocketContext } from './WebSocketContext';
 
-const ws = new WebSocket(process.env.WS_HOST || "ws://localhost:3001");
+console.log(process.env.REACT_APP_WS_HOST);
+const ws = new WebSocket(process.env.REACT_APP_WS_HOST || "");
 
 
 function App() {
   const [state, setState] = useState(State.LOGIN); 
   const StateComponent = states[state];
-
-  
   return <WebSocketContext.Provider value={ws}>
     <StateHandler changeState={setState}/>
     <StateComponent />
